d0c257ce2b5516ee490155cb9a4c2eaa3ed687bf
Emilia Huerta <39370518+Icetalon21@users.noreply.github.com>
2019-09-26 15:18:21 -0700

    Now only prints the 3D model

--- scikitlearn/3D.py
+++ scikitlearn/3D.py
@@ -20,17 +20,19 @@ y = boston_housing_data.target
 #print(y[0],y[0, :])
 
 crime_rate = x[:, 0]
+'''
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.scatter(crime_rate, y)
-
+'''
 nitric_oxide = x[:, 4]
+'''
 ax2 = fig.add_subplot(2, 1, 2)
 ax2.set_title("Nitric Oxide Concentration (PP 10M")
 ax2.set_ylabel('Housing Price')
 ax2.set_xlabel('Nitric Oxide ({})'.format(feat_names[4]))
 ax2.scatter(nitric_oxide, y)
-
+'''
 def standard_norm(x):
     return(x-np.mean(x))/np.std(x)
 
@@ -38,22 +40,22 @@ crime_rate_std = standard_norm(crime_rate)
 nitric_oxide_std = standard_norm(nitric_oxide)
 print(np.min(crime_rate_std), np.max(crime_rate_std))
 print(np.min(nitric_oxide_std), np.max(nitric_oxide_std))
-
+'''
 #STANDARD NORMALIZATION
 fig = plt.figure()
 fig.suptitle('Boston Housing Data')
 ax = fig.add_subplot(1, 1, 1)
 ax.set_ylabel('Housing Price')
 ax.set_xlabel('Standard Norm Features')
-
+'''
 observations_std = (crime_rate_std, nitric_oxide_std)
 targets = (y, y)
 colors = ("blue", "red") # Crime rate will be blue and nitric oxide will be red
 labels = (feat_names[0], feat_names[4])
 markers =('o', '^')
-for obs, tgt, col, lab, m in zip(observations_std, targets, colors, labels, markers):
-    ax.scatter(obs, tgt, c=col, label=lab, marker=m)
-    ax.legend(loc='upper right')
+#for obs, tgt, col, lab, m in zip(observations_std, targets, colors, labels, markers):
+    #ax.scatter(obs, tgt, c=col, label=lab, marker=m)
+    #ax.legend(loc='upper right')
 
 #VISUALIZING IN 3D
 
